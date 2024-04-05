87560dbeeecd0874c1cfe5a351df7b128057d523
Tyler Schloesser <tylerschloesser@gmail.com>
2024-04-05 09:32:37 -0700

    cleanup

--- packages/app/src/world.ts
+++ packages/app/src/world.ts
@@ -1,30 +1,4 @@
-import Prando from 'prando'
-import { CellType, World } from './types.js'
-
-const rng = new Prando(0)
-
-export function getCellColor(type: CellType): string {
-  switch (type) {
-    case CellType.enum.Stone: {
-      const hue = 0
-      const saturation = 0
-      const lightness = 20 + rng.next() * 20
-      return `hsl(${hue}, ${saturation.toFixed(2)}%, ${lightness.toFixed(2)}%)`
-    }
-    case CellType.enum.Grass: {
-      const hue = 120
-      const saturation = 40 + rng.next() * 10
-      const lightness = 20 + rng.next() * 10
-      return `hsl(${hue}, ${saturation.toFixed(2)}%, ${lightness.toFixed(2)}%)`
-    }
-    case CellType.enum.Tree: {
-      const hue = 25
-      const saturation = 45 + rng.next() * 10
-      const lightness = 25 + rng.next() * 10
-      return `hsl(${hue}, ${saturation.toFixed(2)}%, ${lightness.toFixed(2)}%)`
-    }
-  }
-}
+import { World } from './types.js'
 
 export function initWorld(): World {
   const cells: World['cells'] = {}
