781f39b0a8c88c048cc34d29ed7f64bc0004326d
Tyler Schloesser <tylerschloesser@gmail.com>
2024-04-05 12:52:17 -0700

    refactor use camera

--- packages/app/src/use-camera.ts
+++ packages/app/src/use-camera.ts
@@ -1,17 +1,31 @@
-import { useMemo } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { getScale } from './scale.js'
 import { Camera, Cursor } from './types.js'
 import { Vec2 } from './vec2.js'
 
 export function useCamera(
   cursor: Cursor,
-  viewport: Vec2 | null,
+  viewport: Vec2,
 ): Camera {
-  return useMemo(() => {
-    const position = cursor.position
-    const scale = viewport
-      ? getScale(cursor.zoom, viewport.x, viewport.y)
-      : 1
-    return { position, scale }
+  const initial = useInitialCamera(cursor, viewport)
+  const [camera, setCamera] = useState(initial)
+
+  useEffect(() => {
+    setCamera({
+      position: cursor.position,
+      scale: getScale(cursor.zoom, viewport.x, viewport.y),
+    })
   }, [cursor, viewport])
+
+  return camera
+}
+
+function useInitialCamera(cursor: Cursor, viewport: Vec2) {
+  return useMemo(
+    () => ({
+      position: cursor.position,
+      scale: getScale(cursor.zoom, viewport.x, viewport.y),
+    }),
+    [],
+  )
 }
