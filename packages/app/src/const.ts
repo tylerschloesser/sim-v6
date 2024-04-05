0c1f1da16ef30a7d159ce9cd17a316486131c629
Tyler Schloesser <tylerschloesser@gmail.com>
2024-04-05 10:25:27 -0700

    cleanup

--- packages/app/src/const.ts
+++ packages/app/src/const.ts
@@ -1,16 +1,7 @@
 import { Vec2 } from './vec2.js'
 
 export const SHOW_GRID: boolean = true
-export const SHOW_PATH: boolean = true
-export const SHOW_CURSOR: boolean = true
-
 export const ENABLE_SMOOTH_CAMERA: boolean = false
-
-export const MAX_SPEED = Infinity
-
-// How far ahead (in seconds) to simulate the path.
-// The time step cannot be greater than this.
-export const PATH_TIME = 0.5
 
 export function getScale(viewport: Vec2) {
   const vmin = Math.min(viewport.x, viewport.y)
