62823e0b9608621b0d2a677cee0d6d0a4760983b
Tyler Schloesser <tylerschloesser@gmail.com>
2024-04-05 09:44:42 -0700

    add try load world

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -1,13 +1,17 @@
 import { enableMapSet, enablePatches } from 'immer'
 import { createRoot } from 'react-dom/client'
 import invariant from 'tiny-invariant'
+import { ZodError } from 'zod'
 import { App } from './app.js'
 import './index.scss'
+import { loadWorld } from './world.js'
 
 if (localStorage.getItem('reset') === 'true') {
-  console.log('reset')
+  console.debug('reset')
   localStorage.clear()
 }
+
+await tryLoadWorld()
 
 enablePatches()
 enableMapSet()
@@ -16,3 +20,18 @@ const container = document.getElementById('root')
 invariant(container)
 
 createRoot(container).render(<App />)
+
+async function tryLoadWorld() {
+  try {
+    loadWorld()
+  } catch (e) {
+    if (e instanceof ZodError) {
+      if (self.confirm('Failed to parse world. Reset?')) {
+        localStorage.clear()
+        self.location.reload()
+        await new Promise(() => {})
+      }
+    }
+    throw e
+  }
+}
