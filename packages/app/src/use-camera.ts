5ec2216a2c2992fdf5dc4512bff4697cd7fbfcd5
Tyler Schloesser <tylerschloesser@gmail.com>
2024-04-05 13:24:29 -0700

    add transition

--- packages/app/src/use-camera.ts
+++ packages/app/src/use-camera.ts
@@ -1,5 +1,6 @@
 import { useEffect, useMemo, useState } from 'react'
 import { BehaviorSubject, combineLatest } from 'rxjs'
+import { smooth } from './const.js'
 import { getScale } from './scale.js'
 import { Camera, Cursor, Viewport } from './types.js'
 
@@ -8,27 +9,72 @@ export function useCamera(
   viewport$: BehaviorSubject<Viewport>,
 ): Camera {
   const target$ = useTarget$(cursor$, viewport$)
-  const [camera, setCamera] = useState(target$.value)
+  const camera$ = useMemo(
+    () => new BehaviorSubject(target$.value),
+    [],
+  )
+  const [camera, setCamera] = useState(camera$.value)
+
+  useTransition(target$, camera$)
 
   useEffect(() => {
-    let handle: number
-    function step() {
-      setCamera((prev) => {
-        if (target$.value === prev) {
-          return prev
-        }
-        return target$.value
-      })
+    const sub = camera$.subscribe(setCamera)
+    return () => {
+      sub.unsubscribe()
+    }
+  }, [])
 
+  return camera
+}
+
+function useTransition(
+  target$: BehaviorSubject<Camera>,
+  camera$: BehaviorSubject<Camera>,
+): void {
+  useEffect(() => {
+    let handle: number
+    let lastStep = self.performance.now()
+    function step() {
+      const now = self.performance.now()
+      const elapsed = (now - lastStep) / 1000
+      lastStep = now
       handle = self.requestAnimationFrame(step)
+
+      const target = target$.value
+      const camera = camera$.value
+
+      if (target === camera) {
+        return
+      }
+
+      let position = target.position
+      {
+        const d = target.position.sub(camera.position)
+        if (d.len() > 1e-3) {
+          const speed = smooth(d.len(), 3)
+          position = camera.position.add(
+            d.norm().mul(speed * elapsed),
+          )
+        }
+      }
+
+      // const dScale = target$.value.scale - prev.scale
+      const scale = target$.value.scale
+
+      if (
+        position === target.position &&
+        scale === target.scale
+      ) {
+        camera$.next(target)
+      } else {
+        camera$.next({ position, scale })
+      }
     }
     handle = self.requestAnimationFrame(step)
     return () => {
       self.cancelAnimationFrame(handle)
     }
   }, [])
-
-  return camera
 }
 
 function useTarget$(
