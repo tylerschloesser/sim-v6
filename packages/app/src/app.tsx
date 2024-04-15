4e33cfc44f45d73cc6308df229d14d817bec2367
Tyler Schloesser <tylerschloesser@gmail.com>
2024-04-15 13:09:00 -0700

    update styles

--- packages/app/src/app.tsx
+++ packages/app/src/app.tsx
@@ -6,23 +6,28 @@ export function App() {
     <div className="h-dvh flex flex-col-reverse p-4">
       <button
         onClick={() => setCount(count + 1)}
-        className="rounded bg-neutral-500 p-4 font-bold"
+        className="rounded bg-neutral-500 p-2 font-bold text-center"
       >
         Mine Stone
       </button>
       <span className="flex-1 flex flex-col justify-center text-center">
-        <span className="uppercase">Stone</span>
-        <span className="text-2xl">{count}</span>
+        <span className="uppercase text-lg">Stone</span>
+        <span className="text-4xl">{count}</span>
       </span>
-      <div className="text-center border-2 rounded border-neutral-500 p-4 relative">
+      <div className="text-center border-2 rounded border-neutral-500 p-2 relative">
         <span
-          className="absolute left-0 top-0 bottom-0 right-0 bg-green-900 origin-top-left transition-transform"
+          className="absolute w-full h-full top-0 left-0 bg-green-900 origin-top-left transition-transform"
           style={{
             transform: `scaleX(${Math.min(count / 20, 1)})`,
           }}
         ></span>
-        <span className="relative">
-          Goal: Mine 20 Stone
+        <span className="relative flex flex-col">
+          <span className="font-bold">
+            Goal: Mine 20 Stone
+          </span>
+          <span className="text-xs">
+            Tap to see unlocks
+          </span>
         </span>
       </div>
     </div>
