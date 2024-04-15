7a56c3fb37bb3e8adecd9eb54318fd1b98af5301
Tyler Schloesser <tylerschloesser@gmail.com>
2024-04-15 11:23:56 -0700

    add tailwind

--- packages/app/src/index.ts
+++ packages/app/src/index.ts
@@ -1,6 +1,12 @@
 import { html } from 'htm/preact'
 import { render } from 'preact'
 
-render(html`<h1>Hello!</h1>`, document.body)
+function App() {
+  return html`<h1 class="text-3xl font-bold underline">
+    TODO
+  </h1>`
+}
+
+render(html`<${App} />`, document.body)
 
 console.log('done')
---
+++ packages/app/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  jit: true,
+  content: ['./src/**/*.ts'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
