62b9de4385077c43760c717cec967f0e6c13663d
Tyler Schloesser <tylerschloesser@gmail.com>
2024-04-15 09:08:57 -0700

    update

--- packages/app/src/home-v2.tsx
+++ packages/app/src/home-v2.tsx
@@ -1,15 +1,5 @@
-import * as Popover from '@radix-ui/react-popover'
-
 export function HomeV2() {
   return (
-    <Popover.Root>
-      <Popover.Trigger>More info</Popover.Trigger>
-      <Popover.Portal>
-        <Popover.Content>
-          Some more info…
-          <Popover.Arrow />
-        </Popover.Content>
-      </Popover.Portal>
-    </Popover.Root>
+    <h1 className="text-3xl font-bold underline">Todo!</h1>
   )
 }
