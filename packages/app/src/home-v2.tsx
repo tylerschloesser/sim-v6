d76d706ae3fcd21e24106e1ef32ce8414371aefa
Tyler Schloesser <tylerschloesser@gmail.com>
2024-04-15 09:11:32 -0700

    begin parcel refactor

--- packages/app/src/home-v2.tsx
+++
@@ -1,5 +0,0 @@
-export function HomeV2() {
-  return (
-    <h1 className="text-3xl font-bold underline">Todo!</h1>
-  )
-}
--- packages/app/webpack.config.ts
+++
@@ -1,3 +0,0 @@
-import { webpackConfig } from '@tyle/webpack-config'
-
-export default webpackConfig({ tailwind: true })
