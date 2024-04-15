5fd062e1e4d944df2d869f34129475511e04dd37
Tyler Schloesser <tylerschloesser@gmail.com>
2024-04-15 08:48:22 -0700

    add radix

--- packages/app/src/home-v2.tsx
+++ packages/app/src/home-v2.tsx
@@ -1,3 +1,15 @@
+import * as Popover from '@radix-ui/react-popover'
+
 export function HomeV2() {
-  return <>TODO</>
+  return (
+    <Popover.Root>
+      <Popover.Trigger>More info</Popover.Trigger>
+      <Popover.Portal>
+        <Popover.Content>
+          Some more info…
+          <Popover.Arrow />
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
+  )
 }
