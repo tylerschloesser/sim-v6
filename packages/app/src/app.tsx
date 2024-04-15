792fc64c62bbaa0a8958c5c379ffdc5c2c4b2e46
Tyler Schloesser <tylerschloesser@gmail.com>
2024-04-15 12:51:14 -0700

    fix react

--- packages/app/src/app.tsx
+++ packages/app/src/app.tsx
@@ -1,3 +1,20 @@
+import { useState } from 'react'
+
 export function App() {
-  return <h1 className="tx">Hello world! test</h1>
+  const [count, setCount] = useState(0)
+  return (
+    <div className="h-dvh flex flex-col-reverse p-4">
+      <button
+        onClick={() => setCount(count + 1)}
+        className="rounded bg-neutral-500 p-4 font-bold"
+      >
+        Mine Stone
+      </button>
+      <span className="flex-1 flex flex-col justify-center text-center">
+        <span className="uppercase">Stone</span>
+        <span className="text-2xl">{count}</span>
+      </span>
+      <div className="text-center">Goal: Mine 20 Stone</div>
+    </div>
+  )
 }
